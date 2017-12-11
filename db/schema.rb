2e735b106478a2b65ff064a4d6b6e43cb1af5a19
Minh493 <haminh1706@gmail.com>
2017-12-11 10:45:26 +0700

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@ ActiveRecord::Schema.define(version: 20171218032428) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
