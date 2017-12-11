85f54e65a79f0c81769b4e4bf709590655078500
Minh493 <haminh1706@gmail.com>
2017-12-11 10:45:26 +0700

    Add account activation

--- test/helpers/application_helper_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ApplicationHelperTest < ActionView::TestCase
-  test "full title helper" do
-    #assert_equal full_title,         layout links
-    #assert_equal full_title("Help"), layout links
-  end
-end
