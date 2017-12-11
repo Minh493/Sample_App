282c74b2f5cb75f1cb27725f4548320e50b4e4aa
Minh493 <haminh1706@gmail.com>
2017-12-11 10:45:26 +0700

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,16 @@ User.create!(name: "Hà Minh", email: "haminh1706@gmail.com",password: "17061994
   password = "password"
   User.create!(name: name, email: email, password: password, password_confirmation: password,
   activated: true, activated_at: Time.zone.now)
-users = User.order(:created_at).take(6)
+  users = User.order(:created_at).take(6)
 2.times do
   content = Faker::Lorem.sentence(5)
   users.each {|user| user.microposts.create!(content: content)}
   end
 end
+# Following relationships
+  users = User.all
+  user = users.first
+  following = users[2..50]
+  followers = users[3..40]
+  following.each {|followed| user.follow(followed)}
+  followers.each {|follower| follower.follow(user)}
