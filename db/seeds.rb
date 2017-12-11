a0dbb082f062cd02683ac11b51dfd6cd69cf28f9
Minh493 <haminh1706@gmail.com>
2017-12-11 10:45:26 +0700

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
 User.create!(name: "Hà Minh", email: "haminh1706@gmail.com",password: "17061994",
   password_confirmation: "17061994", admin: true,
   activated: true, activated_at: Time.zone.now)
-99.times do |n|
+10.times do |n|
   name  = Faker::Name.name
   email = "ruby-#{n+1}@gmail.com"
   password = "password"
   User.create!(name: name, email: email, password: password, password_confirmation: password,
   activated: true, activated_at: Time.zone.now)
+users = User.order(:created_at).take(6)
+2.times do
+  content = Faker::Lorem.sentence(5)
+  users.each {|user| user.microposts.create!(content: content)}
+  end
 end
