ecf692ca5ea11563beb4603754e205d9245d1a50
Minh493 <haminh1706@gmail.com>
2017-12-11 10:45:26 +0700

    Finish user edit, update, index, and destroy actions

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase
+  test "full title helper" do
+    #assert_equal full_title,         layout links
+    #assert_equal full_title("Help"), layout links
+  end
+end
